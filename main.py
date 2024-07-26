1687decc9fb54650c6360ed2c25031463a1a0076
unknown <jesus.gabriel.mn99@gmail.com>
2024-07-26 16:48:31 -0400

    feature: now the script won't ask for the first file path. Now all source sheets will be added to the main file, with the document name of the file for each sheet

--- main.py
+++ main.py
@@ -1,28 +1,10 @@
 import openpyxl
 import os
+import xlwings as xw
 import tkinter as tk
+import time
+from pathlib import Path
 from tkinter import messagebox, filedialog
-
-
-# This function will open a file explorer and ask to the user to select a file
-def get_file():
-  file_path = tk.filedialog.askopenfilename(
-    title='Seleccione el archivo TechnicalProperties',
-    filetypes=[("Archivos de Excel", "*.xlsx *.xlsm *.xls")]
-  )
-  
-  # Get result from first function and print it
-  evaluation_result = evaluate_file(file_path)
-  print(evaluation_result)
-  
-  # If the operation was cancelled by the user, just return the result
-  if evaluation_result == 'Operation cancelled':
-    return evaluation_result
-  
-  # If the user wants to proceed, execute the second function
-  else:
-    rename_result = rename_sheet(file_path)
-    print(rename_result)
 
 # The function will evaluate the value of the 'Unicode' field
 def evaluate_file(file_path):
@@ -66,7 +48,7 @@ def evaluate_file(file_path):
 def rename_sheet(file_path):
   if not file_path:
     return 'No se encontró ningún archivo'
-  
+
   try:
     # Open the workbook and access the worksheet
     wb = openpyxl.load_workbook(file_path)
@@ -90,11 +72,63 @@ def rename_sheet(file_path):
     
   except Exception as e:
     return f"Error: {str(e)}"
-  
+
+# This function will ask for all the other files and will add them as sheets
+def merge_files(file_path, folder_name, timestamp):
+  if file_path:
+    excel_files = list(Path(folder_name).glob('*.xlsx'))
+    combined_wb = xw.Book()
+
+    for excel_file in excel_files:
+      wb = xw.Book(excel_file)
+      for sheet in wb.sheets:
+        # Obtener el nombre del archivo sin la extensión .xlsx
+        sheet_name = excel_file.stem
+        # Copiar la hoja y asignarle el nombre del archivo
+        sheet.api.Copy(After=combined_wb.sheets[0].api)
+        combined_wb.sheets[1].name = sheet_name
+
+    
+    # Save the combined workbook with a timestamp
+    combined_wb.sheets[0].delete()
+    combined_wb.save(f'merged{timestamp}.xlsx')
+
+    if len(combined_wb.app.books) == 1:
+      combined_wb.app.quit()
+      return 'All files merged successfully'
+    else:
+      combined_wb.close()
+      return 'Error: Failed to merge files'
+
 def main():
   root = tk.Tk()
   root.withdraw()
-  get_file()
+
+  # Get current file path (The excel file should be in the same)
+  script_file_path = os.path.dirname(os.path.abspath(__file__))
+  folder_name = 'documents'
+  file_name = 'TechnicalProperties.xlsx'
+
+  # Get the excel file path
+  file_path = os.path.join(script_file_path, folder_name, file_name)
+
+  t = time.localtime()
+  timestamp = time.strftime('%Y-%m-%d_%H-%M-%S', t)
+
+  # Call the first function and print the result
+  evaluation_result = evaluate_file(file_path)
+  print(evaluation_result)
+  
+  # If the operation was cancelled by the user, just return the result
+  if evaluation_result == 'Operation cancelled':
+    return evaluation_result
+  
+  # If the user wants to proceed, execute the second function
+  else:
+    rename_result = rename_sheet(file_path)
+    print(rename_result)
+
+  merge_files(file_path, folder_name, timestamp)
 
 if __name__ == '__main__':
   main()
